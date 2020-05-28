ebfa43377296e84b9c008b935e68d3bda060a08b
Isabela Ferreira <isabelad-carvalho@hotmail.com>
2020-05-28 19:23:32 -0300

    commit 28/05

---
+++ projetosmartretail/src/components/Cart.vue
@@ -0,0 +1,59 @@
+<template>
+    <div class="row boottom-bar">
+        <img src="../assets/cart.svg" class="cart">
+        <p>SACOLA</p>
+        <div class="itens">3 itens</div>
+    </div>
+</template>
+
+<script>
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+    *{
+        font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+        margin: 0px;
+        padding: 0px;
+    }
+
+    .row{
+        height: 60px;
+        width: 100%;
+        margin: 0px 0px 0px -10px;
+        background-color: #FF906A;
+        position: fixed;
+        top: 89%;
+        box-shadow: 1px -1px 5px -1px #8888 ;
+        justify-content: center;
+        align-items: center;
+    }
+
+    .cart{
+        padding: 10px;
+        width: 50px;
+        height: 50px;
+        border-radius: 10px;
+        margin-right: 2%;
+    }
+
+    .cart:hover{
+        background-color: #ffff;
+        border-radius: 50px;
+    }
+
+    p{
+        font-weight: 500;
+        color: #000;
+    }
+
+    .itens{
+        color: #000;
+        margin-left: 10px;
+        background-color: #fff;
+        padding: 10px;
+        font-weight: 500;
+        border-radius: 50px;
+    }
+ 
+</style>
--- projetosmartretail/src/components/Shop.vue
+++ projetosmartretail/src/components/Shop.vue
@@ -1,61 +1,90 @@
 <template>
-  <div class="container">
-    <h3>LOJA.COM</h3>
+<div class="content">
+  <div class="wrapper">
+    <h3 class="logo">LOJA.COM</h3>
+    <p class="location">Av. Pinheiro Laranja, nº 50</p>
+    <div class="row up-bar">
+        <input type="text" class="search-bar">
+        <img src="../assets/filter.png" class="filter">
+    </div>
     <table>
-        <thead></thead>
-        <tr class="categories">
-            <th>Bebidas</th>
-            <th>Frutas</th>
-            <th>Limpeza</th>
-            <th>Congelados</th>
-        </tr>
+        <thead class="categories">
+            <th><img src="../assets/bottle.svg"><p>Bebidas</p></th>
+            <th><img src="../assets/pineapple.svg"><p>Frutas</p></th>
+            <th><img src="../assets/cleaning.svg"><p>Limpeza</p></th>
+            <th><img src="../assets/fridge.svg"><p>Congelados</p></th>
+        </thead>
         <tr>
             <td>
                 <img class="miniatura" src="../assets/cerveja.png">
                 <div class="text-box">
                     <p class="title">Cerveja</p>
-                    <p class="desc">Lorem Ipsum</p>
+                    <p class="price">R$ 10,00</p>
+                    <p class="promotional">R$ 12,50</p>
                 </div>
-                <img class="btn-add" src="../assets/star.svg">
+                <div class="icons-box">
+                    <div class="btn-favorite"></div>
+                    <div class="btn-add"><div class="qtd"><p>2</p></div></div>
+                </div>
             </td>
             <td>
                 <img class="miniatura" src="../assets/ninho.png">
                 <div class="text-box">
                     <p class="title">Leite ninho</p>
-                    <p class="desc">Lorem Ipsum</p>
+                    <p class="price">R$ 10,00</p>
+                    <p class="promotional">R$ 12,50</p>
                 </div>
-                <button class="btn btn-add">+</button>
+                <div class="icons-box">
+                    <div class="btn-favorite"></div>
+                    <div class="btn-add"></div>
+                </div>
             </td>
             <td>
                 <img class="miniatura" src="../assets/coca.png">
                 <div class="text-box">
                     <p class="title">Coca-cola</p>
-                    <p class="desc">Lorem Ipsum</p>
+                    <p class="price">R$ 10,00</p>
+                    <p class="promotional">R$ 12,50</p>
                 </div>
-               <button class="btn btn-add">+</button> 
+                <div class="icons-box">
+                    <div class="btn-favorite"></div>
+                    <div class="btn-add"></div>
+                </div>
             </td>
             <td>
                 <img class="miniatura" src="../assets/cafe.png">
                 <div class="text-box">
                     <p class="title">Café Pilão</p>
-                    <p class="desc">Lorem Ipsum</p>
+                    <p class="price">R$ 10,00</p>
+                    <p class="promotional">R$ 12,50</p>
                 </div>
-                <button class="btn btn-add">+</button>
+                <div class="icons-box">
+                    <div class="btn-favorite"></div>
+                    <div class="btn-add"></div>
+                </div>
             </td>
         </tr>
-    </table>
-  </div>
+    </table> 
+    </div>
+    <Cart />
+</div>
 </template>
 
 <script>
+import Cart from './Cart.vue';
 import axios from 'axios';
 export default {
+    components: {
+        Cart
+    },
     data () {
     return {
-      info: null
+            info: '' 
         }
     },
+
     mounted () {
+        const usuario =  { UsuarioStr: "prova", SenhaStr: "passprova" };
         const sendData = {
         IdEntrega: 0,
         IdTipoProduto: 0,
@@ -65,10 +94,27 @@ export default {
         NomeProdutoPesquisaStr: '',
         NomeProdutoStr: '',
         OrdemSecao: 0
-      }
+        };
+
       axios
-        .post('https://boardapihomolog.smartretail.app/api/PromocoesGeraisMobile?idLoja=10719&pagina=0&quantidadePorPagina=150', sendData)
-        .then(response => (this.info = response.info))
+        .post('https://boardapihomolog.smartretail.app/api/usuarioapimobile/login', usuario)
+        .then(response => {this.info = localStorage.getItem(JSON.stringify(response.data.data.TokenStr))})
+        .catch(error => console.log(error))
+
+        // const instance = axios.create({
+        // baseURL: 'https://boardapihomolog.smartretail.app/api/PromocoesGeraisMobile?idLoja=10719&pagina=0&quantidadePorPagina=150',
+        // timeout: 1000,
+        // headers: {'Authorization': 'Bearer '+token}
+        // });
+
+        // instance.get('/path')
+        // .then(response => {
+        //     return response.data;
+        // })
+      axios
+        .post('https://boardapihomolog.smartretail.app/api/PromocoesGeraisMobile?idLoja=10719&pagina=0&quantidadePorPagina=150', 
+             {headers:{'Authorization': 'Bearer'+ this.info}}, sendData)
+        .then(response => (console.log(response)))
         .catch(error => console.log(error))
     }
 }
@@ -81,8 +127,74 @@ export default {
         margin: 5px;
     }
 
+    .content{
+        display: flex;
+        flex-direction: column;
+    }
     body{
         background-color: #FAFAFA;
+    }
+    .wrapper{
+        flex: 1;
+        max-height: auto;
+        max-width: 100%;
+        padding-bottom: 50px;
+    }
+
+    .logo{
+        margin-top: 20px;
+        font-family: Arial, Helvetica, sans-serif;
+        font-weight: 990;
+    }
+
+    .location{
+        line-height: 8px;
+        color: #FE7445;
+    }
+
+    .cart{
+        float: right;
+        padding: 10px;
+        width: 50px;
+        height: 50px;
+        border-radius: 10px;
+        margin-right: 5%;
+    }
+
+    .cart:hover{
+        background-color: #DDDDDD;
+    }
+    .up-bar{
+        justify-content: center;
+        padding: 5px;
+    }
+
+    .search-bar{
+        width: 80%;
+        border-radius: 6px;
+        border: 1px solid #EFEFEF;
+        background-color:transparent;
+        background: url("../assets/loupe.png");
+        background-repeat: no-repeat;
+        padding-left: 40px;
+        background-position-x: 10px;
+        background-position-y: center;
+    }
+
+    .search-bar:hover{
+        background-color: #fff;
+    }
+    input:focus{
+        border-radius: 3px;
+        box-shadow: 1px 1px 4px -1px #8888 ;
+        outline-color: #FE7445;
+        background-color: #fff;
+    }
+
+    .filter{
+        width: 30px;
+        height: 30px;
+        margin-left: 2%;
     }
 
     table{
@@ -92,40 +204,82 @@ export default {
     }
 
     td{
-        padding: 30px 20px 20px 20px;
+        padding: 10px 20px 10px 20px;
         display: flex;
         flex-direction: row;
         justify-content: space-between;
         border-radius: 6px;
         box-shadow: 1px 1px 4px -1px #8888 ;
         background-color: #ffff;
+        align-content: center;
     }
 
-    th{
-        background-color: #ffff;
-        color:#F98056;
-        padding: 20px;
+    th {
+        color: #000;
+        display: flex;
+        flex-direction: column;
+        text-align: center;
+    }
+
+    th img{
+        width: 80px;
+        height: 80px;
+        padding: 15px;
         border-radius: 100px;
     }
 
-    th:hover{
-        background-color: #F98056;
-        color:#fff;
+    th img:hover{
+        background-color: #ffff;
+        box-shadow: 0px 0px 5px 1px #FE7445 ;
     }
 
-    tr.categories{
+    thead.categories{
         width: 100%;
         display: flex;
-        justify-content: space-around;
+        padding: 0px 10px 0px 10px;
+        background-color: #FF906A;
+        overflow-x: scroll;
+        border-radius: 30px 0px 0px 30px;
     }
 
+    .btn-favorite{
+        background: url("../assets/heart.png");
+        width: 24px;
+        height: 24px;
+    }
+
+    .btn-favorite:hover{
+        background: url("../assets/hearthover.png");
+    }
+    .btn-favorite:active{
+        background: url("../assets/hearthover.png");
+    }
     .btn-add{
-        color: #8888;
-        font-size: 18px;
-        font-weight: bold;
-        border-radius: 50%;
-        width: 30px;
-        height: 30px;
+        background: url("../assets/plus.png");
+        width: 24px;
+        height: 24px;
+        align-content: center;
+        display: flex;
+        flex-wrap: wrap;
+    }
+
+    .btn-add:hover{
+        background: url("../assets/plushover.png");
+        width: 24px;
+        height: 24px;
+    }
+
+    .qtd{
+        width: 24px;
+        height: 24px;
+        background-color: #ffffff;
+        border-radius: 5px 0px 0px 5px;
+        box-shadow: -2px 0px 4px -1px #8888;
+        margin-left: -98%;
+        font-weight: 500;
+        text-align: center;
+        align-content: center;
+        justify-content: center;
     }
 
     .miniatura{
@@ -136,11 +290,31 @@ export default {
     .text-box{
         line-height: 8px;
         background-color: transparent;
-
+        align-self: center;
     }
 
-    .desc{
-        font-weight: 400;
-        color: #898989;
+    .price{
+        font-size:18px;
+        font-weight: 600;
+        color: #000;
+    }
+    .promotional{
+        color: #F77070;
+        text-decoration: line-through;  
+        font-size:14px;
+        font-weight: 500;
+    }
+
+    .icons-box{
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+    }
+
+    .bottom{
+        background-color: #FFF;
+        width: 100%;
+        margin: 0px;
+        padding: 0px;
     }
 </style>
